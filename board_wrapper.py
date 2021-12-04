7206e0475999be34e1a0a71c893b68b1e4ab3c97
Alon <alonvbk@gmail.com>
2021-12-04 17:50:15 +0200

    revert back to network with batch_sz

--- board_wrapper.py
+++ board_wrapper.py
@@ -184,7 +184,7 @@ def load_prev(run: namedtuple, i: int, args: dict):
         df = pd.DataFrame()
         network: ModelBase = get_model(run.model_type, args['vocab_sz'], run.dropout,
                                        args['layers_num'], args['hidden_layer_units'],
-                                       args['weights_uniforming'], run.seq_sz)
+                                       args['weights_uniforming'], args['batch_sz'])
         epoch_start = 0
     return network, df, epoch_start
 
@@ -240,7 +240,11 @@ def background_train(i: int, run: namedtuple, criterion, args: dict, epochs: int
             # Embedding layer weights check for possible explode
             embedding_weight_check(network, i)
             # Track losses for TB
+<<<<<<< HEAD
             m.track_loss(loss, train=1)
+=======
+            m.track_loss(loss / network.batch_sz, train=1)
+>>>>>>> parent of 72212cc (important! change in model dimensions batch_sz -> seq_sz)
 
         # Same run for Test only [without backprop]
         ###########################################################################
@@ -252,7 +256,11 @@ def background_train(i: int, run: namedtuple, criterion, args: dict, epochs: int
             preds, states = network(x, states)
             loss = criterion(preds, y.view(-1, 1))
             # Track losses for TB
+<<<<<<< HEAD
             m.track_loss(loss, train=0)
+=======
+            m.track_loss(loss / network.batch_sz, train=0)
+>>>>>>> parent of 72212cc (important! change in model dimensions batch_sz -> seq_sz)
 
         m.end_epoch(network, device)
         # Save results to csv & json files + Model
